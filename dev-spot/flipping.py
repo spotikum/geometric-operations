818ac9e2c0428e97be95be717fe4e748f5207612
Indra Wahyu <spot@tracert.id>
2022-10-11 17:49:17 +0800

    add horizon

--- dev-spot/flipping.py
+++ dev-spot/flipping.py
@@ -8,6 +8,10 @@ original_img = Image.open("image.jpeg")
 vertical_img = original_img.transpose(method=Image.FLIP_TOP_BOTTOM)
 vertical_img.save("output/vertical.png")
 
+# Flip the original image horizontally
+horz_img = original_img.transpose(method=Image.FLIP_LEFT_RIGHT)
+horz_img.save("output/horizontal.png")
+
 # close all our files object
 original_img.close()
 vertical_img.close()
