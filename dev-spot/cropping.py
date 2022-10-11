593f103b3f15a80737379138b9ffc37595e96522
Indra Wahyu <spot@tracert.id>
2022-10-11 18:05:22 +0800

    cropping is ready with bug cannot close :)

---
+++ dev-spot/cropping.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread("image.jpeg")
+print(type(img))
+
+# Shape of the image
+print("Shape of the image", img.shape)
+
+# [rows, columns]
+crop = img[50:180, 100:300]
+
+cv2.imshow('original', img)
+cv2.imshow('cropped', crop)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
